311df167b9e54d7d94faf78750e6d402332c099c
Faiyaz Sarkar <faiyazvai999@gmail.com>
2026-01-02 14:49:21 +0600

    update env file to include next public url

--- src/lib/eden-client.ts
+++ src/lib/eden-client.ts
@@ -1,5 +1,5 @@
 import { App } from "@/app/api/[[...slugs]]/route";
 import { treaty } from "@elysiajs/eden";
 
-const app = treaty<App>("localhost:3000");
+const app = treaty<App>(process.env.NEXT_PUBLIC_API_URL!);
 export default app;
